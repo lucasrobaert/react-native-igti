a1c580091af5c44d2e9563d51589c17f32cf0791
Lucas Robaert <lucasrobaert@hotmail.com>
2020-08-02 13:46:39 -0300

    eslint e themeProvider

---
+++ src/hooks/theme.js
@@ -0,0 +1,59 @@
+import React, {createContext, useState, useEffect} from 'react';
+import {ThemeProvider} from 'styled-components/native';
+import {Appearance} from 'react-native';
+import {darkTheme, lightTheme} from '../styles/theme';
+import {storeValue, retriveValue} from '../utils/localStorage';
+import {Platform} from 'react-native';
+
+const defaultMode = Appearance.getColorScheme() === 'no-preference' ? 'light' : Appearance.getColorScheme();
+
+const ThemeContext = createContext({
+    mode: defaultMode,
+    setMode: (mode) => console.log(mode),
+}); 
+
+export const useTheme = () => React.useContext(ThemeContext);
+
+export const ManageThemeProvider = ({children} ) => {
+    const [themeState, setThemeState] = useState(defaultMode);
+
+
+    const setMode = (mode) =>{
+        setThemeState(mode);
+        storeValue('themeMode', mode);
+    };
+
+
+    useEffect(() => {
+        let themeMode;
+
+        const themeAsync = async () => {
+            const majorVersionIOS = parseInt(Platform.Version, 10);
+
+            themeMode = await retriveValue('themeMode');
+
+            if((majorVersionIOS < 13 || Platform.Version < 29) && themeMode){
+                setThemeState(themeMode);
+            }
+        };
+
+        const subscription = Appearance.addChangeListener(({colorScheme}) =>{
+            setThemeState(colorScheme);
+        });
+
+        themeAsync();
+
+        return () => subscription.remove();
+    },[]);
+
+    
+    return(
+        <ThemeContext.Provider value={{mode: themeState, setMode}}>
+            <ThemeProvider theme={themeState === 'dark' ? darkTheme.theme : lightTheme.theme}>
+                {children}
+            </ThemeProvider>
+        </ThemeContext.Provider>
+    )
+
+
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,30 @@
+export const lightTheme = {
+    theme: {
+      background: '#eeeeee',
+      rootBackground: '#fcfdff',
+      cardBackground: '#ffffff',
+      cardBorder: '#ede8df',
+      text: '#527318',
+      subText: '#2a7886',
+      border: '#ede8df',
+      shadowEffect: '#dfdccd',
+      circle: '#ffffff',
+      horizontalBorder: '#ede8df',
+    },
+  };
+  
+  export const darkTheme = {
+    theme: {
+      background: '#393e46',
+      rootBackground: '#393e46',
+      cardBackground: '#232931',
+      cardBorder: 'transparent',
+      text: '#4ecca3',
+      horizontalBorder: '#545151',
+      subText: '#8ac6d1',
+      shadowEffect: 'transparent',
+      circle: '#232931',
+      border: 'transparent',
+    },
+  };
+  
